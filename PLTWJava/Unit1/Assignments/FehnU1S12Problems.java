da656bbb419b0263c82f08f9439a5972bf33d821
loganhue52 <logan.fehn@evsck12.com>
2021-09-20 22:38:32 -0500

    u1s12

--- PLTWJava/Unit1/Assignments/FehnU1S12Problems.java
+++ PLTWJava/Unit1/Assignments/FehnU1S12Problems.java
@@ -1,17 +1,44 @@
 package PLTWJava.Unit1.Assignments;
+import java.util.Scanner;
+import java.lang.Math;
 
 public class FehnU1S12Problems {
     public static void main(String[] args){
-        triangleArea();
+        // triangleArea();
         midPointTable();
         minuteYearConverter();
         triangleVolume();
     }
     private static void triangleArea(){
-        
+        Scanner ui = new Scanner(System.in);
+        System.out.print("x1: ");
+        double x1=ui.nextDouble();
+        System.out.print("y1: ");
+        double y1=ui.nextDouble();
+        System.out.print("x2: ");
+        double x2=ui.nextDouble();
+        System.out.print("y2: ");
+        double y2=ui.nextDouble();
+        System.out.print("x3: ");
+        double x3=ui.nextDouble();
+        System.out.print("y3: ");
+        double y3=ui.nextDouble();
+        double side1 = Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2, 2));
+        double side2 = Math.sqrt(Math.pow(x2-x3,2)+Math.pow(y2-y3, 2));
+        double side3 = Math.sqrt(Math.pow(x3-x1,2)+Math.pow(y3-y1, 2));
+        double s = (side1+side2+side3)/2;
+        double area = Math.sqrt(s*(s-side1)*(s-side2)*(s-side3));
+        System.out.printf("Area: %.2f",area);
+        ui.close();
     }
     private static void midPointTable(){
-
+        // SCOTT METHOD FOR THE WIN
+        double ax1=0; double ay1=0; double ax2=1; double ay2=4; double ax3=2; double ay3=7; double ax4=3; double ay4=9; double ax5=4; double ay5=11;
+        double bx1=2; double by1=1; double bx2=4; double by2=2; double bx3=6; double by3=3; double bx4=10; double by4=5; double bx5=12; double by5=7;
+        double mx1=(ax1+bx1)/2;
+        double my1=(ay1+by1)/2;
+        System.out.println("a\tb\tMiddle Point");
+        System.out.printf("(%f, %f)\t(%f, %f)\t(%f, %f)",ax1,ay1,bx1,by1,mx1,my1);
     }
     private static void minuteYearConverter(){
 
