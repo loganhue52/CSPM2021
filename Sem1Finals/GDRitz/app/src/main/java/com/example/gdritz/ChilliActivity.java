321132adaaee383c0ec2fca055752cd3200a1880
loganhue52 <logan.fehn@evsck12.com>
2021-12-08 14:05:46 -0600

    gdritz

---
+++ Sem1Finals/GDRitz/app/src/main/java/com/example/gdritz/ChilliActivity.java
@@ -0,0 +1,139 @@
+package com.example.gdritz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ChilliActivity extends AppCompatActivity {
+    //Side Button
+    private Button iceCreamBTN;
+    private Button comboBTN;
+    private Button sandwichBTN;
+    private Button hotDogsBTN;
+    private Button chiliBTN;
+    private Button specialBTN;
+    private Button saladBTN;
+    private Button checkoutBTN;
+
+    // Order Buttons
+    private Button oneBTN;
+    private Button twoBTN;
+    private Button threeBTN;
+    private Button fourBTN;
+    private Button fiveBTN;
+    private Button sixBTN;
+    private Button sevenBTN;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chilli_activity);
+
+        //Side Button
+        iceCreamBTN = findViewById(R.id.iceCreamBTN);
+        comboBTN = findViewById(R.id.combosBTN);
+        sandwichBTN = findViewById(R.id.sandwichBTN);
+        hotDogsBTN = findViewById(R.id.hotDogsBTN);
+        chiliBTN = findViewById(R.id.chiliBTN);
+        specialBTN = findViewById(R.id.specialBTN);
+        saladBTN = findViewById(R.id.saladBTN);
+        checkoutBTN = findViewById(R.id.checkOutBTN);
+
+        // Order Button
+        oneBTN = findViewById(R.id.oneBTN);
+        twoBTN = findViewById(R.id.twoBTN);
+        threeBTN = findViewById(R.id.threeBTN);
+        fourBTN = findViewById(R.id.fourBTN);
+
+
+        //Side buttons
+        iceCreamBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent i = new Intent(SandwichActivity.this,IceCreamActivity.class);
+                //startActivity(i);
+            }
+        });
+        comboBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ChilliActivity.this,ComboActivity.class);
+                startActivity(i);
+            }
+        });
+        sandwichBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent i = new Intent(SandwichActivity.this,SandwichActivity.class);
+//                startActivity(i);
+            }
+        });
+        hotDogsBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent i = new Intent(SandwichActivity.this,HotDogsActivity.class);
+                //startActivity(i);
+            }
+        });
+        chiliBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent i = new Intent(SandwichActivity.this,ChiliActivity.class);
+//                startActivity(i);
+                Toast.makeText(getApplicationContext(), "You're already on the Chili Menu!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        specialBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Intent i = new Intent(SandwichActivity.this,SpecialActivity.class);
+                //startActivity(i);
+            }
+        });
+        saladBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent i = new Intent(SandwichActivity.this,SaladActivity.class);
+//                startActivity(i);
+            }
+        });
+        checkoutBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ChilliActivity.this,CheckoutActivity.class);
+                startActivity(i);
+            }
+        });
+
+        // Order Button
+        oneBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        twoBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        threeBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        fourBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
